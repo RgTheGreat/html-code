3ffd1523dda41c7a2a0b60e0fd01786bb3a4fb41
Programmer <71826040+RgTheGreat@users.noreply.github.com>
2020-12-29 16:18:57 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -8,3 +8,20 @@ document.getElementById("Demo").innerHTML = x + y;
 var d = new Date();
 d.setFullYear(2020);
 document.getElementById("demo").innerHTML = d;
+
+
+var d = new Date();
+d.setHours(22);
+document.getElementById("demo").innerHTML = d;
+
+
+
+
+var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
